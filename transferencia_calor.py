7944ef8c54281e4b255908107bd855540d07541c
daniel19066 <dpallaresr@unal.edu.co>
2021-07-18 17:43:42 -0500

    ya listo primer excel

--- transferencia_calor.py
+++ transferencia_calor.py
@@ -2,6 +2,7 @@
 import math
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 raiz= Tk()#creacion interfaz
 
 miFrame=Frame(raiz,width=500,height=400)#tamaño inicial
@@ -48,35 +49,35 @@ rdfinal2texto=StringVar()
 deltaptexto=StringVar()
 cuadroTexto0=Entry(miFrame)
 cuadroTexto0.grid(row=0,column=1,padx=10,pady=10)
-nombreLabel0=Label(miFrame, text='Masa Fluido frio:')
+nombreLabel0=Label(miFrame, text='Masa Fluido frio(lb/h):')
 nombreLabel0.grid(row=0,column=0,padx=10,pady=10)
 cuadroTexto1=Entry(miFrame)
 cuadroTexto1.grid(row=1,column=1,padx=10,pady=10)
-nombreLabel1=Label(miFrame, text='Masa Fluido caliente:')
+nombreLabel1=Label(miFrame, text='Masa Fluido caliente(lb/h):')
 nombreLabel1.grid(row=1,column=0,padx=10,pady=10)
 cuadroTexto2=Entry(miFrame)
 cuadroTexto2.grid(row=2,column=1,padx=10,pady=10)
-nombreLabel2=Label(miFrame, text='cp caliente:')
+nombreLabel2=Label(miFrame, text='cp caliente(btu/lb°F):')
 nombreLabel2.grid(row=2,column=0,padx=10,pady=10)
 cuadroTexto3=Entry(miFrame)
 cuadroTexto3.grid(row=4,column=1,padx=10,pady=10)
-nombreLabel3=Label(miFrame, text='temparatura 1 de fluido caliente:')
+nombreLabel3=Label(miFrame, text='temparatura 1 de fluido caliente(°F):')
 nombreLabel3.grid(row=4,column=0,padx=10,pady=10)
 cuadroTexto4=Entry(miFrame)
 cuadroTexto4.grid(row=5,column=1,padx=10,pady=10)
-nombreLabel4=Label(miFrame, text='temperatura 2 de fluido caliente:')
+nombreLabel4=Label(miFrame, text='temperatura 2 de fluido caliente(°F):')
 nombreLabel4.grid(row=5,column=0,padx=10,pady=10)
 cuadroTexto5=Entry(miFrame)
 cuadroTexto5.grid(row=6,column=1,padx=10,pady=10)
-nombreLabel5=Label(miFrame, text='temperatura 1 fluido frio:')
+nombreLabel5=Label(miFrame, text='temperatura 1 fluido frio(°F):')
 nombreLabel5.grid(row=6,column=0,padx=10,pady=10)
 cuadroTexto6=Entry(miFrame)
 cuadroTexto6.grid(row=7,column=1,padx=10,pady=10)
-nombreLabel6=Label(miFrame, text='temperatura 2 fluido frio:')
+nombreLabel6=Label(miFrame, text='temperatura 2 fluido frio(°F):')
 nombreLabel6.grid(row=7,column=0,padx=10,pady=10)
 cuadroTexto7=Entry(miFrame)
 cuadroTexto7.grid(row=3,column=1,padx=10,pady=10)
-nombreLabel7=Label(miFrame, text='cp fluido frio:')
+nombreLabel7=Label(miFrame, text='cp fluido frio(btu/lb°F):')
 nombreLabel7.grid(row=3,column=0,padx=10,pady=10)
 cuadroTexto8=ttk.Combobox(miFrame,values=[
                                     "1", 
@@ -86,7 +87,7 @@ cuadroTexto8=ttk.Combobox(miFrame,values=[
                                     "3",
                                     "4"],state="readonly")
 cuadroTexto8.grid(row=8,column=1,padx=10,pady=10)
-nombreLabel8=Label(miFrame, text='diametro tubo :')
+nombreLabel8=Label(miFrame, text='diametro tubo (cedula 40):')
 nombreLabel8.grid(row=8,column=0,padx=10,pady=10)
 cuadroTexto9=ttk.Combobox(miFrame,values=[
                                     "1", 
@@ -96,19 +97,19 @@ cuadroTexto9=ttk.Combobox(miFrame,values=[
                                     "3",
                                     "4"],state="readonly")
 cuadroTexto9.grid(row=9,column=1,padx=10,pady=10)
-nombreLabel9=Label(miFrame, text='diametro anulo:')
+nombreLabel9=Label(miFrame, text='diametro anulo(cedula 40):')
 nombreLabel9.grid(row=9,column=0,padx=10,pady=10)
 cuadroTexto10=Entry(miFrame)
 cuadroTexto10.grid(row=10,column=1,padx=10,pady=10)
-nombreLabel10=Label(miFrame, text='densidad fluido tubo:')
+nombreLabel10=Label(miFrame, text='densidad fluido tubo(lb/ft^3):') 
 nombreLabel10.grid(row=10,column=0,padx=10,pady=10)
 cuadroTexto11=Entry(miFrame)
 cuadroTexto11.grid(row=11,column=1,padx=10,pady=10)
-nombreLabel11=Label(miFrame, text='viscosidad fluido tubo:')
+nombreLabel11=Label(miFrame, text='viscosidad fluido tubo(cp):')
 nombreLabel11.grid(row=11,column=0,padx=10,pady=10)
 cuadroTexto12=Entry(miFrame)
 cuadroTexto12.grid(row=12,column=1,padx=10,pady=10)
-nombreLabel12=Label(miFrame, text='conductividad fluido tubo:')
+nombreLabel12=Label(miFrame, text='conductividad fluido tubo(btu/ft*h*°F):')
 nombreLabel12.grid(row=12,column=0,padx=10,pady=10)
 cuadroTexto13=Entry(miFrame)
 cuadroTexto13.grid(row=13,column=1,padx=10,pady=10)
@@ -116,11 +117,11 @@ nombreLabel13=Label(miFrame, text='gravedad especifica anulo:')
 nombreLabel13.grid(row=13,column=0,padx=10,pady=10)
 cuadroTexto14=Entry(miFrame)
 cuadroTexto14.grid(row=14,column=1,padx=10,pady=10)
-nombreLabel14=Label(miFrame, text='viscosidad fluido anulo:')
+nombreLabel14=Label(miFrame, text='viscosidad fluido anulo(cp):')
 nombreLabel14.grid(row=14,column=0,padx=10,pady=10)
 cuadroTexto15=Entry(miFrame)
 cuadroTexto15.grid(row=15,column=1,padx=10,pady=10)
-nombreLabel15=Label(miFrame, text='conductividad fluido anulo:')
+nombreLabel15=Label(miFrame, text='conductividad fluido anulo(btu/ft*h*°F):')
 nombreLabel15.grid(row=15,column=0,padx=10,pady=10)
 cuadroTexto16=Entry(miFrame)
 cuadroTexto16.grid(row=16,column=1,padx=10,pady=10)
@@ -202,25 +203,84 @@ nombreLabel21.grid(row=14,column=3,padx=10,pady=10)
 
 #---------------funcion que llama el boton para calcular todo-----------#
 def codigoBoton():
+    #condicional para evitar que haya mas de uno vacio al tiempo
+    if( (cuadroTexto0.get()=='' and cuadroTexto1.get()=='') or (cuadroTexto0.get()=='' and cuadroTexto3.get()=='') or (cuadroTexto0.get()=='' and cuadroTexto4.get()=='')or (cuadroTexto0.get()=='' and cuadroTexto5.get()=='') or(cuadroTexto0.get()=='' and  cuadroTexto6.get()=='') or(cuadroTexto1.get()=='' and cuadroTexto3.get()=='') or (cuadroTexto1.get()=='' and cuadroTexto4.get()=='') or (cuadroTexto1.get()=='' and cuadroTexto5.get()=='') or (cuadroTexto1.get()=='' and cuadroTexto6.get()=='') or (cuadroTexto3.get()=='' and cuadroTexto4.get()=='') or (cuadroTexto3.get()=='' and cuadroTexto5.get()=='')or (cuadroTexto3.get()=='' and cuadroTexto6.get()=='')or (cuadroTexto4.get()=='' and cuadroTexto5.get()=='') or (cuadroTexto4.get()=='' and cuadroTexto6.get()=='') or (cuadroTexto5.get()=='' and cuadroTexto6.get()=='')):
+        messagebox.showwarning('','no puede haber mas de una variable de temperaturas y masas vacia al tiempo')
+        return None
     
-    masa1 = float(cuadroTexto1.get())#100000 #float(input("Ingrese masa1: "))
+    masa1 =0
+    masa2=0
+    if(cuadroTexto2.get()==''):
+        messagebox.showwarning('','el cp del fluido caliente no puede estar vacio')
+        return None
+
+    if(cuadroTexto7.get()==''):
+        messagebox.showwarning('','el cp del fluido frio no puede estar vacio')
+        return None
+
+    
     cp1 = float(cuadroTexto2.get())#0.44 #float(input("Ingrese cp1: "))
     cp2 = float(cuadroTexto7.get()) #0.48 #float(input("Ingrese cp2: "))
-    th1 = float(cuadroTexto3.get())#325 #float(input("Ingrese Th1: "))
-    th2 =  float(cuadroTexto4.get())#275 #float(input("Ingrese Th2: "))
-    tc1 = float(cuadroTexto5.get())#100 #float(input("Ingrese Tc1: "))
-    tc2 =  float(cuadroTexto6.get())#300 #float(input("Ingrese Tc2: "))
+    if(cuadroTexto0.get()==''):
+        Q=abs(float(cuadroTexto1.get())*cp1*(float(cuadroTexto4.get())-float(cuadroTexto3.get())))
+        masa2=Q/abs(cp2*(float(cuadroTexto6.get())-float(cuadroTexto5.get())))
+    else:
+        masa2=float(cuadroTexto0.get())
+    
+
+    if(cuadroTexto1.get()==''):
+        Q=abs(float(cuadroTexto0.get())*cp2*(float(cuadroTexto6.get())-float(cuadroTexto5.get())))
+        masa1=Q/abs(cp1*(float(cuadroTexto4.get())-float(cuadroTexto3.get())))
+    else:
+        masa1=float(cuadroTexto1.get())
+
+    
+    th1 = 0#325 #float(input("Ingrese Th1: "))
+    if(cuadroTexto3.get()==''):
+        Q=abs(float(cuadroTexto0.get())*cp2*(float(cuadroTexto6.get())-float(cuadroTexto5.get())))
+        th1=float(cuadroTexto4.get())+(Q/(cp1*float(cuadroTexto1.get())))
+    else:
+        th1=float(cuadroTexto3.get())
+        
+        
+    th2 =  0#275 #float(input("Ingrese Th2: "))
+    if(cuadroTexto4.get()==''):
+        Q=abs(float(cuadroTexto0.get())*cp2*(float(cuadroTexto6.get())-float(cuadroTexto5.get())))
+        th2=float(cuadroTexto3.get())-(Q/(cp1*float(cuadroTexto1.get())))
+    else:
+        th2=float(cuadroTexto4.get())
+
+    tc1 = 0#100 #float(input("Ingrese Tc1: "))
+    if(cuadroTexto5.get()==''):
+        Q=abs(float(cuadroTexto1.get())*cp1*(float(cuadroTexto4.get())-float(cuadroTexto3.get())))
+        tc1=float(cuadroTexto6.get())-(Q/(cp2*float(cuadroTexto0.get())))
+    else:
+        tc1=float(cuadroTexto5.get())
+    tc2 =  0#300 #float(input("Ingrese Tc2: "))
+    if(cuadroTexto6.get()==''):
+        Q=abs(float(cuadroTexto1.get())*cp1*(float(cuadroTexto4.get())-float(cuadroTexto3.get())))
+        tc2=float(cuadroTexto5.get())+(Q/(cp2*float(cuadroTexto0.get())))
+    else:
+        tc2=float(cuadroTexto6.get())
     calorQ = abs(masa1*cp1*(th2-th1))
-    masa2 = abs(calorQ/(cp2*(tc2-tc1)))
+    
     variabletexto.set(str(calorQ))
     print(calorQ)
 
     # 1) Calcular la LMTD
-    lmtd = ((th2-tc1)-(th1-tc2))/math.log((th2-tc1)/(th1-tc2))
+    lmtd = (abs(th2-tc1)-abs(th1-tc2))/math.log(abs(th2-tc1)/abs(th1-tc2))
     print(lmtd)
     lmtdtexto.set(str(lmtd))
 
     # 3) Seleccionar el diametro interior y exterior segun el nominal
+    if(cuadroTexto8.get()==''):
+        messagebox.showwarning('','el diametro del tubo no puede estar vacio')
+        return None
+    
+    if(cuadroTexto9.get()==''):
+        messagebox.showwarning('','el diametro del anulo no puede estar vacio')
+        return None
+
     diametro1 = cuadroTexto8.get() #input("Ingrese diametro1: ")
     diametro2 = cuadroTexto9.get() #input("Ingrese diametro2: ")
     diamint1, diamext1 = calcDiametro(diametro1)
@@ -233,6 +293,16 @@ def codigoBoton():
     dcomita = diamint2 - diamext1
 
     # 5) Calculo de las propiedades del fluido en tubo
+    if(cuadroTexto10.get()==''):
+        messagebox.showwarning('','la densidad del fluido del tubo no puede estar vacio')
+        return None
+    if(cuadroTexto11.get()==''):
+        messagebox.showwarning('','la viscosidad del fluido del tubo no puede estar vacio')
+        return None
+    if(cuadroTexto12.get()==''):
+        messagebox.showwarning('','la conductividad del fluido del tubo no puede estar vacio')
+        return None
+    
     densidad1 = float(cuadroTexto10.get())
     viscosidadcp1 = float(cuadroTexto11.get())
     viscosidadft1 = viscosidadcp1 * 2.42
@@ -246,6 +316,16 @@ def codigoBoton():
     hinttexto.set(str(hint))
 
     # 6) Calculo de las propiedades del fluido en el anulo
+    if(cuadroTexto13.get()==''):
+        messagebox.showwarning('','la gravedad especifica del fluido del anulo no puede estar vacio')
+        return None
+    if(cuadroTexto14.get()==''):
+        messagebox.showwarning('','el cp del fluido frio no puede estar vacio')
+        return None
+    if(cuadroTexto15.get()==''):
+        messagebox.showwarning('','la conductividad del fluido del anulo no puede estar vacio')
+        return None
+    
     gravespe = float(cuadroTexto13.get())
     densidad2 = gravespe * 62.43
     viscosidadcp2 = float(cuadroTexto14.get())
@@ -260,6 +340,19 @@ def codigoBoton():
     hexttexto.set(str(hext))
 
     # 7) Calculo del area - longitudes, como L es la misma para tubo y anulo
+    if(cuadroTexto16.get()==''):
+        messagebox.showwarning('','el rd del tubo no puede estar vacio pero si puede ser 0')
+        return None
+    if(float(cuadroTexto16.get())<0.00):
+        messagebox.showwarning('','el rd del tubo no puede ser menor a 0')
+        return None
+
+    if(cuadroTexto17.get()==''):
+        messagebox.showwarning('','el numero de horquillas no puede estar vacio')
+        return None
+    
+
+
     he = (hext * diamext1) / diamint1
     uclean1 = 1/((1/hint)+(1/he))
     print(uclean1)
@@ -283,6 +376,17 @@ def codigoBoton():
     rdfinal1 = (1/udisecorregido1) - (1/uclean1)
     print(rdfinal1)
     rdfinal1texto.set(str(rdfinal1))
+
+    if(cuadroTexto18.get()==''):
+        messagebox.showwarning('','el rd del anulo no puede estar vacio pero si puede ser 0')
+        return None
+    if(float(cuadroTexto18.get())<0.00):
+        messagebox.showwarning('','el rd del anulo no puede ser menor a 0')
+        return None
+
+    if(cuadroTexto19.get()==''):
+        messagebox.showwarning('','el numero de horquillas no puede estar vacio')
+        return None
 
     hi = (hint*diamint1)/diamext1
     uclean2 = 1/((1/hext)+(1/hi))
@@ -317,6 +421,7 @@ def codigoBoton():
     deltap = deltaf*densidad1
     print(deltap)
     deltaptexto.set(str(deltap))
+    print('xs')
 
 botonCalcular=Button(raiz,text='enviar',command=codigoBoton)
 botonCalcular.pack()
